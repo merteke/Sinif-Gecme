0f082fd7d8e6b2fab6a83f9c410bd164b3e04af5
Mert Eke <125980514+merteke@users.noreply.github.com>
2024-03-15 16:08:36 +0300

    Add files via upload

---
+++ sinifGecme.java
@@ -0,0 +1,59 @@
+import java.util.*;
+
+
+public class sinifGecme {
+
+    public static void main(String[] args){
+
+        Scanner input=new Scanner(System.in);
+        
+        double mat,fizik,turkce,kimya,muzik;
+
+        double toplam=0;
+        double ort;
+
+        System.out.println("Matematik notunu giriniz");
+        mat=input.nextDouble();
+        System.out.println("Fizik notunu giriniz");
+        fizik=input.nextDouble();
+        System.out.println("Turkce notunu giriniz");
+        turkce=input.nextDouble();
+        System.out.println("Kimya notunu giriniz");
+        kimya=input.nextDouble();
+        System.out.println("Muzik notunu giriniz");
+        muzik=input.nextDouble();
+
+        if(mat<=100 && mat>=0){
+            toplam+=mat;
+        }
+        if(fizik<=100 && fizik>=0){
+            toplam+=fizik;
+            
+        }
+        if(kimya<=100 && kimya>=0){
+            toplam+=kimya;
+            
+        }
+        if(turkce<=100 && turkce>=0){
+            toplam+=turkce;
+            
+        }
+        if(muzik<=100 && muzik>=0){
+            toplam+=muzik;
+            
+        }
+
+        ort=toplam/5;
+
+        System.out.println("Ogrencinin genel ortalamasi="+ort);
+
+        if(ort>=55){
+            System.out.println("Ogrenci sinifi basariyla gecti!");
+
+        }
+        else{
+            System.out.println("Ogrenci sinifta kaldi!");
+        }
+    }
+
+}
